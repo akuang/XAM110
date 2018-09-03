dd889c572c5d02d67bf4d0dc04cffb4b120e4559
Alex Kuang <akuang@microsoft.com>
2018-09-02 20:33:38 -0700

    Add Shared project 'MyTunes.Shared' and use it in iOS & Android

--- Exercise 1/Start/MyTunes.Droid/MainActivity.cs
+++ Exercise 1/Start/MyTunes.Droid/MainActivity.cs
@@ -1,20 +1,29 @@
 ﻿using Android.App;
 using Android.OS;
+using MyTunes.Shared;
+using System.Linq;
 
 namespace MyTunes
 {
 	[Activity(Label = "@string/app_name", MainLauncher = true, Icon = "@drawable/icon")]
 	public class MainActivity : ListActivity
 	{
-		protected override void OnCreate(Bundle bundle)
+		protected override async void OnCreate(Bundle bundle)
 		{
 			base.OnCreate(bundle);
+            //ListAdapter = new ListAdapter<string>() {
+            //	DataSource = new[] { "One", "Two", "Three" }
+            //};
 
-			ListAdapter = new ListAdapter<string>() {
-				DataSource = new[] { "One", "Two", "Three" }
-			};
+            SongLoader songLoader = new SongLoader();
+            var songList = await songLoader.Load();
+
+            ListAdapter = new ListAdapter<Song>()
+            {
+                DataSource = songList.ToList(),
+                TextProc = song => song.Name,
+                DetailTextProc = song => $"{song.Artist} - {song.Album}"
+            };
 		}
 	}
 }
-
-
--- Exercise 1/Start/MyTunes.iOS/MyTunesViewController.cs
+++ Exercise 1/Start/MyTunes.iOS/MyTunesViewController.cs
@@ -1,4 +1,6 @@
 using UIKit;
+using System.Linq;
+using MyTunes.Shared;
 
 namespace MyTunes
 {
@@ -11,15 +13,25 @@ namespace MyTunes
 			TableView.ContentInset = new UIEdgeInsets (20, 0, 0, 0);
 		}
 
-		public override void ViewDidLoad()
+		public override async void ViewDidLoad()
 		{
 			base.ViewDidLoad();
 
-			TableView.Source = new ViewControllerSource<string>(TableView) {
-				DataSource = new string[] { "One", "Two", "Three" },
-			};
-		}
-	}
+            //TableView.Source = new ViewControllerSource<string>(TableView) {
+            //	DataSource = new string[] { "One", "Two", "Three" },
+            //};
+
+            SongLoader songLoader = new SongLoader();
+            var songList = await songLoader.Load();
+
+            TableView.Source = new ViewControllerSource<Song>(TableView)
+            {
+                DataSource = songList.ToList(),
+                TextProc = song => song.Name,
+                DetailTextProc = song => $"{song.Artist} - {song.Album}" 
+            };
+        }
+    }
 
 }
 
